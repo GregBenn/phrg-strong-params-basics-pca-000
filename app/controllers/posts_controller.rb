27acf2eff993b249eef40099dc18a2ad11b012e5
GregBenn <gbenn07@gmail.com>
2018-08-01 14:54:19 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,19 +13,67 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+# ---------------------------------
+  # original
+
+  # def create
+  #   @post = Post.new(params.require(:post).permit(:title, :description))
+  #   @post.save
+  #   redirect_to post_path(@post)
+  # end
+
+  # def update
+  #   @post = Post.find(params[:id])
+  #   @post.update(params.require(:post).permit(:title))
+  #   redirect_to post_path(@post)
+  # end
+
+# ---------------------------------
+# refactored to use private method
+
+  # def create
+  #   @post = Post.new(post_params)
+  #   @post.save
+  #   redirect_to post_path(@post)
+  # end
+
+  # def update
+  #   @post = Post.find(params[:id])
+  #   @post.update(post_params)
+  #   redirect_to post_path(@post)
+  # end
+
+  # private
+
+  # def post_params
+  #   params.require(:post).permit(:title, :description)
+  # end
+
+# ---------------------------------
+# refactored to allow title and description to be created, but only title to be updated
   def create
-    @post = Post.new(params["post"])
+    @post = Post.new(post_params(:title, :description))
     @post.save
     redirect_to post_path(@post)
   end
 
   def update
     @post = Post.find(params[:id])
-    @post.update(params["post"])
+    @post.update(post_params(:title))
     redirect_to post_path(@post)
   end
 
-  def edit
-    @post = Post.find(params[:id])
+  private
+
+  # We pass the permitted fields in as *args;
+  # this keeps `post_params` pretty dry while
+  # still allowing slightly different behavior
+  # depending on the controller action
+  def post_params(*args)
+    params.require(:post).permit(*args)
   end
 end
